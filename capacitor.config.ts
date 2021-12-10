c11c96daa65fff6667609b47004f8727e7fa2b1b
Terence Waters <Terence.Waters@imail.org>
2021-12-09 18:47:02 -0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'terence-waters-ionic',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
