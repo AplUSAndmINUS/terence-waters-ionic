6cd7b92e1ea1acf488f49cfe7688a59b555c96b9
Terence Waters <Terence.Waters@imail.org>
2021-12-09 20:04:48 -0700

    updated stylesheets to SCSS

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+
 import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import './Home.scss';
 
 const Home: React.FC = () => {
   return (
