ec59fbaa9b784c1f4c49f33f01891609f1d21c2c
Terence Waters <12157591+AplUSAndmINUS@users.noreply.github.com>
2021-12-13 20:31:34 -0700

    adding menu and menu button components

---
+++ src/components/menu/MenuButton/MenuButton.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { menuOutline } from 'ionicons/icons';
+
+export const MenuButton: React.FC = () => {
+  return (
+    <h1>I am a menu button!</h1>
+  )
+};
+
+export default MenuButton;
---
+++ src/components/menu/MenuMobile.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const MenuMobile: React.FC = () => {
+  return (
+    <h1>I am the menu on small devices!</h1>
+  )
+};
+
+export default MenuMobile;
