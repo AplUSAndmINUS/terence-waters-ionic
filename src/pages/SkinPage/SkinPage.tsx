9518fb4ad91281e2e43a1b01460c695a872b7bf6
Terence Waters <12157591+AplUSAndmINUS@users.noreply.github.com>
2021-12-13 21:43:00 -0700

    initial add of ionic grid and columns for content on skin page

--- src/pages/SkinPage/SkinPage.tsx
+++ src/pages/SkinPage/SkinPage.tsx
@@ -1,4 +1,5 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonCol, IonContent, IonHeader, IonPage, IonRow, IonToolbar } from '@ionic/react';
+import MenuButton from '../../components/menu/MenuButton/MenuButton';
 
 import './SkinPage.scss';
 
@@ -6,11 +7,17 @@ export const SkinPage: React.FC = () => {
   return (
     <IonPage>
       <IonContent fullscreen>
-        <IonHeader>
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+        <IonHeader className="ion-no-border">
+          <IonToolbar className="ion-no-border" style={{ position: 'fixed', top: 0, right: 0 }}>
+            <MenuButton /> 
           </IonToolbar>
         </IonHeader>
+        <IonRow>
+          <IonCol className="ion-align-items-center ion-justify-content-center">
+            <IonRow className="ion-align-items-center ion-justify-content-center" style={{ height: '50vh'}}>Six</IonRow>
+            <IonRow className="ion-align-items-center ion-justify-content-center" style={{ height: '50vh'}}>hi there, I'm Terence Waters</IonRow>
+          </IonCol>
+        </IonRow>
       </IonContent>
     </IonPage>
   );
