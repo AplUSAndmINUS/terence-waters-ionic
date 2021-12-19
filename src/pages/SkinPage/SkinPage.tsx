a57c75cfd39976f5cb56b8f9661f120e444e32a6
Terence Waters <12157591+AplUSAndmINUS@users.noreply.github.com>
2021-12-19 13:25:21 -0700

    updated styling for buttons and image reflection for design balance

--- src/pages/SkinPage/SkinPage.tsx
+++ src/pages/SkinPage/SkinPage.tsx
@@ -1,4 +1,11 @@
-import { IonCol, IonContent, IonHeader, IonPage, IonToolbar } from '@ionic/react';
+import {
+  IonButton,
+  IonCol,
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonToolbar
+} from '@ionic/react';
 import MenuButton from '../../components/menu/MenuButton/MenuButton';
 
 import './SkinPage.scss';
@@ -13,18 +20,25 @@ export const SkinPage: React.FC = () => {
             <MenuButton /> 
           </IonToolbar>
         </IonHeader>
-        <FullScreenRow className="ion-hide-lg-up ion-align-items-center ion-justify-content-end">
+        <FullScreenRow className="ion-hide-lg-up ion-align-items-center ion-justify-content-end ion-padding">
           <IonCol size="12">&nbsp;</IonCol>
-          <IonCol size="12" className="">
+          <IonCol size="12" className="ion-padding">
             <h3 className="ion-text-center">hi there, I'm</h3>
             <h1 className="ion-text-center">Terence Waters</h1>
             <h3 className="ion-text-center">ui/ux designer, <br />web developer &amp; tutor</h3>
             <button className="ion-center">hire me!</button>
           </IonCol>
         </FullScreenRow>
-        <FullScreenRow className="ion-justify-content-center ion-hide-lg-down">
-          <IonCol className="ion-align-items-center ion-justify-content-center ion-hide-sm-down">hi there, I'm Terence Waters</IonCol>
-          <IonCol>Six Medium screen</IonCol>
+        <FullScreenRow className="ion-justify-content-center ion-align-items-center ion-hide-lg-down ion-padding">
+          <IonCol className="ion-padding">
+            <h3 className="ion-text-center">hi there, I'm</h3>
+            <h1 className="ion-text-center ion-margin-bottom">Terence Waters</h1>
+            <div className="ion-justify-content-center ion-align-items-center">
+              <h3 className="ion-text-center">ui/ux designer, <br />web developer &amp; tutor</h3>
+              <IonButton color="primary">hire me!</IonButton>
+            </div>
+          </IonCol>
+          <IonCol>&nbsp;</IonCol>
         </FullScreenRow>
       </IonContent>
     </IonPage>
